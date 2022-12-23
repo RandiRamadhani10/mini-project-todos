aae4fc0bc2ceee61d7abfd2b8e7808aa12a034bb
RandiRamadhani10 <80814841+RandiRamadhani10@users.noreply.github.com>
2022-12-24 03:50:03 +0700

    add assets & utils

---
+++ src/assets/icons/index.js
@@ -0,0 +1,10 @@
+import IC_Delete from "./ic-delete.svg";
+import IC_Delete_Active from "./ic-delete-IC_Delete_active.svg";
+import IC_Edit from "./ic-edit.svg";
+import IC_Edit_Active from "./ic-edit-active.svg";
+import IC_Left from "./ic-left.svg";
+import IC_Left_Active from "./ic-left-Active.svg";
+import IC_Right from "./ic-Right.svg";
+import IC_Right_Active from "./ic-Right-Active.svg";
+
+export { IC_Delete, IC_Delete_Active, IC_Edit, IC_Edit_Active, IC_Left, IC_Left_Active, IC_Right, IC_Right_Active };
