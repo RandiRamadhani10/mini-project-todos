4f9c83c8c9ff670e763bad4ca06f9bcd45e101e7
RandiRamadhani10 <80814841+RandiRamadhani10@users.noreply.github.com>
2022-12-24 05:14:28 +0700

    add BaseButton

---
+++ src/components/atoms/BaseInput/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.css";
+import "../../../assets/styles/styles.css";
+const BaseInput = () => {
+  return <input type="text" value={this.state.value} onChange={this.handleChange} />;
+};
+
+export default BaseInput;
---
+++ src/pages/Login.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { BaseButton } from "../components/atoms";
+const Login = () => {
+  return <div></div>;
+};
+
+export default Login;
